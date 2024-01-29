899f470316610f86f2b8451b8eae0c2b9b9b018e
maciek@arch <mcyran1996@gmail.com>
2024-01-29 23:28:21 +0100

    lesson 252

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -40,4 +40,13 @@ export class AppComponent {
       'list-group-item-danger': server.status === 'critical'
     };
   }
+
+  onAddServer() {
+    this.servers.push({
+      instanceType: 'small',
+      name: 'New server',
+      status: 'stable',
+      started: new Date(14, 5,1996)
+    })
+  }
 }
